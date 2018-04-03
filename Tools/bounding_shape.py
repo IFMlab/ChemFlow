ab8c4f217968736bcd6e902d7196dc038c0c4e01
cbouy <bouysset.cedric@gmail.com>
2018-04-03 17:08:55 +0200

    implemented bounding box for vina

--- Tools/bounding_shape.py
+++ Tools/bounding_shape.py
@@ -4,12 +4,21 @@ from __future__ import print_function
 from math import sqrt
 import sys, re
 
-# Usage : python bounding_sphere.py file.mol2
+# Usage : python bounding_sphere.py file.mol2 --vina
 # This script will read a mol2 file and return the center and radius of the smallest sphere containing all the atoms
+
+def help():
+	print("Usage: python bounding_shape.py file.mol2 --box/--sphere")
+	print("--sphere returns 4 values separated by a space: x, y, z coordinates of the center and radius of the sphere")
+	print("--box returns 6 values: x, y, z coordinates of the center, and size along the x, y, z dimensions")
 
 def distance(a,b):
 	'''Euclidian distance between 2 points'''
 	return sqrt(sum([(xa-xb)**2 for xa,xb in zip(a,b)]))
+
+def distance_along_axis(a,b, axis=0):
+	'''Distance between 2 points along an axis'''
+	return abs(a[axis]-b[axis])
 
 def get_center(points):
 	'''Centroid for finite number of points'''
@@ -37,7 +46,7 @@ def average_bs(XYZ):
 				if i == len(XYZ)-1:
 					found = True
 					break
-	print(' '.join(['{:.4f}'.format(x) for x in center+[radius]]))
+	return center, radius
 
 def ritter_bs(XYZ):
 	"""Bounding sphere using Ritter's algorithm"""
@@ -84,12 +93,32 @@ def ritter_bs(XYZ):
 				if i == len(XYZ)-1:
 					found = True
 					break
-	print(' '.join(['{:.4f}'.format(x) for x in center+[radius]]))
+	return center, radius
+
+def bounding_box(XYZ):
+	# get center
+	center = get_center(XYZ)
+	# get furthest point for each axis
+	size = []
+	for axis in range(3):
+		dmax = 0
+		for point in XYZ:
+			d = distance_along_axis(center, point, axis=axis)
+			if d > dmax:
+				dmax = d
+		size.append(dmax)
+	return center, size
 
 if __name__ == '__main__':
-	if len(sys.argv) != 2:
-		print("Illegal number of arguments")
+	if len(sys.argv) != 3:
+		help()
 		sys.exit()
+	elif any(h in sys.argv for h in ['-h','--help']):
+		help()
+		sys.exit()
+
+	# print commands to show bounding box on pymol
+	pymol = False
 
 	# Read file
 	this_file = open(sys.argv[1], "r")
@@ -117,5 +146,33 @@ if __name__ == '__main__':
 	    data = lines[line].split()
 	    XYZ.append([ float(data[i]) for i in [2,3,4] ])
 
-	# get bounding sphere
-	average_bs(XYZ)
+	# compute bounding shape
+	if sys.argv[2] == '--box':
+		center, size = bounding_box(XYZ)
+		print(' '.join(['{:.3f}'.format(x) for x in center + size]))
+		if pymol:
+			print('pseudoatom a1, pos={}'.format([center[0]+size[0],center[1]+size[1],center[2]+size[2]]))
+			print('pseudoatom a2, pos={}'.format([center[0]+size[0],center[1]+size[1],center[2]-size[2]]))
+			print('pseudoatom a3, pos={}'.format([center[0]+size[0],center[1]-size[1],center[2]-size[2]]))
+			print('pseudoatom a4, pos={}'.format([center[0]+size[0],center[1]-size[1],center[2]+size[2]]))
+			print('pseudoatom a5, pos={}'.format([center[0]-size[0],center[1]-size[1],center[2]+size[2]]))
+			print('pseudoatom a6, pos={}'.format([center[0]-size[0],center[1]-size[1],center[2]-size[2]]))
+			print('pseudoatom a7, pos={}'.format([center[0]-size[0],center[1]+size[1],center[2]-size[2]]))
+			print('pseudoatom a8, pos={}'.format([center[0]-size[0],center[1]+size[1],center[2]+size[2]]))
+			print('distance d12, a1, a2')
+			print('distance d23, a2, a3')
+			print('distance d34, a3, a4')
+			print('distance d14, a1, a4')
+			print('distance d56, a5, a6')
+			print('distance d67, a6, a7')
+			print('distance d78, a7, a8')
+			print('distance d85, a8, a5')
+			print('distance d18, a1, a8')
+			print('distance d72, a7, a2')
+			print('distance d54, a5, a4')
+			print('distance d63, a3, a6')
+	elif sys.argv[2] == '--sphere':
+		center, radius = average_bs(XYZ)
+		print(' '.join(['{:.3f}'.format(x) for x in center + [radius]]))
+	else:
+		help()
