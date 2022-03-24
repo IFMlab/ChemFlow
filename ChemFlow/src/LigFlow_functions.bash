898a77a63530cce99d4ba3a902af7fb47a6d556a
Diego Gomes <dgomes@live.com>
2022-03-23 23:56:49 -0500

    Fixed LigFlow. Devs, please test rigorously I want to remove all the obsolete code.

--- ChemFlow/src/LigFlow_functions.bash
+++ ChemFlow/src/LigFlow_functions.bash
@@ -15,6 +15,15 @@ LigFlow_sanity() {
     fi
 }
 
+LigFlow_update_list() {
+for LIGAND in ${LIGAND_LIST[@]} ; do
+    if [ ! -f ${RUNDIR}/${CHARGE}/${LIGAND}.mol2 ] ; then
+        UPDATE_LIST+=("${LIGAND}")
+    fi
+done
+LIGAND_LIST=${UPDATE_LIST}
+}
+
 LigFlow_prepare() {
     
     # HPC adjustments
@@ -33,7 +42,14 @@ LigFlow_prepare() {
 }
 
 run_LigFlow_prepare () {
- 
+
+  if [ ! -d ${RUNDIR}/${CHARGE} ] ; then
+    mkdir -p ${RUNDIR}/${CHARGE}
+  fi
+
+  for LIGAND in ${LIGAND_LIST[@]} ; do
+
+    echo ${LIGAND}
     # Create tmp directory
     tmp_dir=$(mktemp -d -t ligflow-XXXXXXXXXX)
     
@@ -55,13 +71,13 @@ run_LigFlow_prepare () {
                     -fo gcrt -o ligand.gau  \
                     -ge ligand.gesp \
                     -ch ligand -eq 1 -gv 1 \
-                    -gm %mem=${MAXMEM}Gb -gn %nproc=${NCPUS} \
+                    -gm %mem=2Gb -gn %nproc=$(nproc --all) \
                     -rn MOL -dr no -pf y
 
         if [ -f ligand.gau ] ; then 
             g09 <ligand.gau>ligand.gout
 
-            # If gaussian ended normally
+            # If gaussian ended normally, post-process Gaussian Output to produce final .mol2
             if [ "$(awk '/Normal/' ligand.gout )" != '' ] ; then
                 antechamber -fi gout -i ligand.gout  \
                             -fo mol2 -o resp.mol2 \
@@ -76,7 +92,7 @@ run_LigFlow_prepare () {
     if [ -f ${CHARGE}.mol2 ] ; then
 
       # Copy results
-      cp ${CHARGE}.mol2 $OUT_FOLDER/${LIGAND}.mol2
+      cp ${CHARGE}.mol2 ${RUNDIR}/${CHARGE}/${LIGAND}.mol2
 
       # Clean up tmp files.
       rm -rf ${tmp_dir}
@@ -90,25 +106,24 @@ Check output at ${tmp_dir}
 "
 
     fi
+  done
 }
 
 run_LigFlow_prepare_HPC() {
     # Writes content of this function to stdout
 
     # Copy HPC header to run file
-    cat ${HPC_HEADER}
+    cat ${HEADER_FILE}
 
     # Write out relevant variables
     echo "
 #######################################
 # Config
 #######################################
+RUNDIR=\"${RUNDIR}\"
 CHARGE=\"${CHARGE}\"
 LIGAND=\"${LIGAND}\"
 LIGAND_FILE=\"${LIGAND_FILE}\"
-OUT_FOLDER=\"${OUT_FOLDER}\"
-NCPUS=\"${NCPUS}\"
-MAXMEM=\"${MAXMEM}\"
 
 #######################################
 # Functions
@@ -126,26 +141,203 @@ run_LigFlow_prepare
 }
 
 
+LigFlow_summary() {
+#===  FUNCTION  ================================================================
+#          NAME: DockFlow_summary
+#   DESCRIPTION: Summarize all docking information
+#
+#    PARAMETERS: ${HOSTNAME}
+#                ${USER}
+#                ${PROJECT}
+#                ${PROTOCOL}
+#                ${PWD}
+#                ${LIGAND_FILE}
+#                ${NLIGANDS}
+#                ${JOB_SCHEDULLER}
+#                ${NCORES}
+#       RETURNS: -
+#
+#===============================================================================
+echo "\
+LigFlow summary:
+-------------------------------------------------------------------------------
+[ General info ]
+    HOST: ${HOSTNAME}
+    USER: ${USER}
+ PROJECT: ${PROJECT}
+ WORKDIR: ${WORKDIR}
+
+[ Setup ]
+  LIGAND FILE: $(relpath "${LIGAND_FILE}"   "${WORKDIR}")
+     NLIGANDS: ${NLIGANDS}
+
+[ Charge options ]
+    BCC: ${BCC}
+   RESP_gaff2: ${RESP_GAFF2}
+   RESP_sybyl: ${RESP_SYBYL}
+
+[ Run options ]
+JOB SCHEDULLER: ${JOB_SCHEDULLER}
+    CORES/NODE: ${NCORES}
+        HEADER: ${HEADER_FILE}
+"
+
+echo -n "
+Continue [y/n]? "
+read opt
+case $opt in
+"Y"|"YES"|"Yes"|"yes"|"y")  ;;
+*)  echo "Exiting" ; exit 0 ;;
+esac
+}
+
+
+LigFlow_help() {
+echo "Example usage:
+LigFlow -l ligand.sdf -p myproject [--bcc] [--resp][--resp_sybyl] --charges-file mychargefile.dat
+or
+LigFlow -l ligand.mol2 -p myproject [--bcc] [--resp][--resp_sybyl] 
+
+[Options]
+ -h/--help           : Show this help message and quit
+ -hh/--full-help      : Detailed help
+
+ -l/--ligand         : Ligands .mol2 input file.
+ -p/--project        : ChemFlow project.
+"
+exit 0
+}
+
+
+LigFlow_help_full(){
+echo "LigFlow is a bash script designed to prepare the ligand for DockFlow and ScoreFlow.
+
+Usage:
+LigFlow -l ligand.sdf -p myproject [--bcc] [--resp] [--resp_sybyl]
+
+[Help]
+ -h/--help              : Show this help message and quit
+ -hh/--full-help         : Detailed help
+
+[ Required ]
+*-p/--project       STR : ChemFlow project
+*-l/--ligand       FILE : Ligands  sdf file
+
+[ Optional ]
+ --bcc                  : Compute bcc charges
+ --resp                 : Compute resp charges with gaff2 atom types
+ --resp_sybyl           : Compute resp charges with sybyl atom types
+
+[ Parallel execution ]
+ -nc/--cores        INT : Number of cores per node [${NCORES}]
+ --pbs/--slurm          : Workload manager, PBS or SLURM
+ --header          FILE : Header file provided to run on your cluster.
+
+[ Development ] 
+ --charges-file    FILE : Contains the net charges for all ligands in a library.
+                          ( name charge )  ( CHEMBL123 -1 ) 
+
+"
+    exit 0
+}
+
+
+LigFlow_CLI() {
+if [ "$1" == "" ] ; then
+    ERROR_MESSAGE="LigFlow called without arguments."
+    ChemFlow_error ;
+fi
+
+while [[ $# -gt 0 ]]; do
+    key="$1"
+
+    case ${key} in
+        "-h"|"--help")
+            LigFlow_help
+            exit 0
+        ;;
+        "-hh"|"--full-help")
+            LigFlow_help_full
+            exit 0
+        ;;
+        "-l"|"--ligand")
+            LIGAND_FILE=$(abspath "$2")
+            #echo "$LIGAND_FILE"
+            IFS=. read var1 end_file <<< $LIGAND_FILE
+            echo $end_file
+            shift # past argument
+        ;;
+        "-p"|"--project")
+            PROJECT="$2"
+            shift
+        ;;
+        # Charges
+        "--bcc")
+            BCC="yes"
+            CHARGE="bcc"
+	    RESP_GAFF2="no"
+	    RESP_SYBYL="no"
+        ;;
+        "--resp")
+            BCC="no"
+            RESP="yes"
+            CHARGE="resp"
+            ATOM_TYPE="gaff2"
+	    RESP_GAFF2="yes"
+	    RESP_SYBYL="no"
+        ;;
+	"--resp_sybyl")
+            BCC="no"
+            RESP="yes"
+            CHARGE="resp"
+            ATOM_TYPE="sybyl"
+	    RESP_SYBYL="yes"  
+	    RESP_GAFF2="no"
+        ;;
+        "-nc"|"--cores") # Number of Cores [1] (or cores/node)
+            NCORES="$2" # Same as above.
+            #NC_CHANGED="yes"
+            shift # past argument
+        ;;
+        # HPC options
+        "--pbs") #Activate the PBS workload
+            JOB_SCHEDULLER="PBS"
+        ;;
+        "--slurm") #Activate the SLURM workload
+            JOB_SCHEDULLER="SLURM"
+        ;;
+        "--header")
+            HEADER_PROVIDED="yes"
+            HEADER_FILE=$(abspath "$2")
+            shift
+        ;;
+        # Features under Development 
+        "--charges-file")
+        #   CHARGE_FILE=$(abspath "$2")
+        #   if [ ! -f ${CHARGE_FILE} ] ; then echo "Charge file \"${CHARGE_FILE}\" not found " ;  exit 1 ; fi
+        #   shift
+        ;;
+        *)
+            unknown="$1"        # unknown option
+            echo "Unknown flag \"$unknown\""
+        ;;
+    esac
+    shift # past argument or value
+done
+}
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#######################################################
+# For the honor and glory of our lord Jesus !!!       #
+#                                                     #
+# Everything bellow is overly complicated.            #
+#                                                     #
+# Please ChemBase deserves a better implementation    #
+#                                                     #
+# Let's go back to Keeping Things Super Simple (KISS) #
+#######################################################
 
 
 
@@ -563,188 +755,4 @@ case ${JOB_SCHEDULLER} in
 # ###
 ;;
 esac
-}
-
-
-LigFlow_summary() {
-#===  FUNCTION  ================================================================
-#          NAME: DockFlow_summary
-#   DESCRIPTION: Summarize all docking information
-#
-#    PARAMETERS: ${HOSTNAME}
-#                ${USER}
-#                ${PROJECT}
-#                ${PROTOCOL}
-#                ${PWD}
-#                ${LIGAND_FILE}
-#                ${NLIGANDS}
-#                ${JOB_SCHEDULLER}
-#                ${NCORES}
-#       RETURNS: -
-#
-#===============================================================================
-echo "\
-LigFlow summary:
--------------------------------------------------------------------------------
-[ General info ]
-    HOST: ${HOSTNAME}
-    USER: ${USER}
- PROJECT: ${PROJECT}
- WORKDIR: ${WORKDIR}
-
-[ Setup ]
-  LIGAND FILE: $(relpath "${LIGAND_FILE}"   "${WORKDIR}")
-     NLIGANDS: ${NLIGANDS}
-
-[ Charge options ]
-    BCC: ${BCC}
-   RESP_gaff2: ${RESP_GAFF2}
-   RESP_sybyl: ${RESP_SYBYL}
-
-[ Run options ]
-JOB SCHEDULLER: ${JOB_SCHEDULLER}
-    CORES/NODE: ${NCORES}
-$(abspath "$2")
-"
-
-echo -n "
-Continue [y/n]? "
-read opt
-case $opt in
-"Y"|"YES"|"Yes"|"yes"|"y")  ;;
-*)  echo "Exiting" ; exit 0 ;;
-esac
-}
-
-
-LigFlow_help() {
-echo "Example usage:
-LigFlow -l ligand.sdf -p myproject [--bcc] [--resp][--resp_sybyl] --charges-file mychargefile.dat
-or
-LigFlow -l ligand.mol2 -p myproject [--bcc] [--resp][--resp_sybyl] 
-
-[Options]
- -h/--help           : Show this help message and quit
- -hh/--full-help      : Detailed help
-
- -l/--ligand         : Ligands .mol2 input file.
- -p/--project        : ChemFlow project.
-"
-exit 0
-}
-
-
-LigFlow_help_full(){
-echo "LigFlow is a bash script designed to prepare the ligand for DockFlow and ScoreFlow.
-
-Usage:
-LigFlow -l ligand.sdf -p myproject [--bcc] [--resp] [--resp_sybyl]
-
-[Help]
- -h/--help              : Show this help message and quit
- -hh/--full-help         : Detailed help
-
-[ Required ]
-*-p/--project       STR : ChemFlow project
-*-l/--ligand       FILE : Ligands  sdf file
-
-[ Optional ]
- --bcc                  : Compute bcc charges
- --resp                 : Compute resp charges with gaff2 atom types
- --resp_sybyl           : Compute resp charges with sybyl atom types
-
-[ Parallel execution ]
- -nc/--cores        INT : Number of cores per node [${NCORES}]
- --pbs/--slurm          : Workload manager, PBS or SLURM
- --header          FILE : Header file provided to run on your cluster.
-
-[ Development ] 
- --charges-file    FILE : Contains the net charges for all ligands in a library.
-                          ( name charge )  ( CHEMBL123 -1 ) 
-
-"
-    exit 0
-}
-
-
-LigFlow_CLI() {
-if [ "$1" == "" ] ; then
-    ERROR_MESSAGE="LigFlow called without arguments."
-    ChemFlow_error ;
-fi
-
-while [[ $# -gt 0 ]]; do
-    key="$1"
-
-    case ${key} in
-        "-h"|"--help")
-            LigFlow_help
-            exit 0
-        ;;
-        "-hh"|"--full-help")
-            LigFlow_help_full
-            exit 0
-        ;;
-        "-l"|"--ligand")
-            LIGAND_FILE=$(abspath "$2")
-            #echo "$LIGAND_FILE"
-            IFS=. read var1 end_file <<< $LIGAND_FILE
-            echo $end_file
-            shift # past argument
-        ;;
-        "-p"|"--project")
-            PROJECT="$2"
-            shift
-        ;;
-        # Charges
-        "--bcc")
-            BCC="yes"
-            CHARGE="bcc"
-	    RESP_GAFF2="no"
-	    RESP_SYBYL="no"
-        ;;
-        "--resp")
-            RESP="yes"
-            CHARGE="resp"
-            ATOM_TYPE="gaff2"
-	    RESP_GAFF2="yes"
-	    RESP_SYBYL="no"
-        ;;
-	"--resp_sybyl")
-            RESP="yes"
-            CHARGE="resp"
-            ATOM_TYPE="sybyl"
-	    RESP_SYBYL="yes"  
-	    RESP_GAFF2="no"
-        ;;
-        "-nc"|"--cores") # Number of Cores [1] (or cores/node)
-            NCORES="$2" # Same as above.
-            #NC_CHANGED="yes"
-            shift # past argument
-        ;;
-        # HPC options
-        "--pbs") #Activate the PBS workload
-            JOB_SCHEDULLER="PBS"
-        ;;
-        "--slurm") #Activate the SLURM workload
-            JOB_SCHEDULLER="SLURM"
-        ;;
-        "--header")
-            HEADER_PROVIDED="yes"
-            HEADER_FILE=$(abspath "$2")
-            shift
-        ;;
-        # Features under Development 
-        "--charges-file")
-        #   CHARGE_FILE=$(abspath "$2")
-        #   if [ ! -f ${CHARGE_FILE} ] ; then echo "Charge file \"${CHARGE_FILE}\" not found " ;  exit 1 ; fi
-        #   shift
-        ;;
-        *)
-            unknown="$1"        # unknown option
-            echo "Unknown flag \"$unknown\""
-        ;;
-    esac
-    shift # past argument or value
-done
 }
