78e0c76d32d2839fdc7c7d1ba51204ab6428b86d
kgalentino <55707215+kgalentino@users.noreply.github.com>
2022-05-23 09:35:49 +0200

    Update LigFlow_functions.bash

--- ChemFlow/src/LigFlow_functions.bash
+++ ChemFlow/src/LigFlow_functions.bash
@@ -68,6 +68,8 @@ run_LigFlow_prepare () {
     # Extract one molecule from a .mol2 file
     awk -v id=${LIGAND} -v line='@<TRIPOS>MOLECULE' 'BEGIN {print line}; $0~id{flag=1} /MOLECULE/{flag=0} flag'  ${LIGAND_FILE} > ligand.mol2
 
+    LC_ALL=C
+	
     net_charge=$(awk '{q+=$9}END{printf ("%1.0f\n", q)};/@<TRIPOS>BOND/{exit}' ligand.mol2 )
 
      echo -e "\e[33m
